f15642aac45871c6077dbe516debd3a64d10a0cf
Rafael Alamañac Garrido <zerintia_ralamanac@sacyr.com>
2021-06-10 16:10:38 +0200

    Commit with mongoose

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,24 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from 'src/coffees/coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Coffee, CoffeeSchema } from './entities/coffee.entity';
+import { EventSchema } from 'src/events/entities/event.entity';
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+@Module({
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Coffee.name,
+        schema: CoffeeSchema,
+      },
+      {
+        name: Event.name,
+        schema: EventSchema,
+      },
+    ]),
+  ],
+})
 export class CoffeesModule {}
